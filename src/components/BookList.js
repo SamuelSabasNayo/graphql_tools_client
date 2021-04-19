db45f7c8449eaa9294f083f960f2a94af9f2ae8c
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-04-19 21:51:31 +0200

    rendering all books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,38 @@
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getBookQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 export class BookList extends Component {
+  displayBook() {
+    const data = this.props;
+
+    if (data.data.loading) {
+      return <div>Loading books...</div>;
+    }
+    return data.data.books.map(book => (
+      <li key={book.id}>{book.name}</li>
+    ));
+  }
+
   render() {
     return (
       <div>
         <ul id="book-list">
-          <li>Book name</li>
+          { this.displayBook() }
         </ul>
       </div>
     );
   }
 }
 
-export default BookList;
+export default graphql(getBookQuery)(BookList);
