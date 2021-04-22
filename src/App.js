52f2d045a1a3e9e12fa2f689e2a67f7ad88a17f5
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-04-22 22:45:20 +0200

    retrieving authors and books

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
@@ -14,6 +15,7 @@ export class App extends Component {
         <div id="main">
           <h1>Book List</h1>
           <BookList />
+          <AddBook />
         </div>
       </ApolloProvider>
     );
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,12 @@
-/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
-import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
-
-const getBookQuery = gql`
-  {
-    books {
-      name
-      id
-    }
-  }
-`;
+import { getBooksQuery } from '../querries/querries';
 
 export class BookList extends Component {
   displayBook() {
     const data = this.props;
 
-    if (data.data.loading) {
-      return <div>Loading books...</div>;
-    }
+    if (data.data.loading) return <div>Loading books...</div>;
     return data.data.books.map(book => (
       <li key={book.id}>{book.name}</li>
     ));
@@ -35,4 +23,4 @@ export class BookList extends Component {
   }
 }
 
-export default graphql(getBookQuery)(BookList);
+export default graphql(getBooksQuery)(BookList);
